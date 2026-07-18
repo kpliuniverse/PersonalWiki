57cc25610afc379fa0edacdebe2a71e9562e3e08
kpliuniverse <lolitalaroasas@gmail.com>
2026-07-18 17:20:27 +0800

    qweb and text

--- src/mainapp/src/app.py
+++ src/mainapp/src/app.py
@@ -1,22 +1,41 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QTextEdit
+
+from PyQt6.QtWidgets import QApplication, QSplitter, QWidget, QGridLayout, QTextEdit
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import QUrl
 
 class App:
+    """
+        The main app class
+    """
     app: QApplication 
     widget: QWidget
+    root_layout: QGridLayout
+
     def __init__(self):
         self.app = QApplication(sys.argv)
-        self.widget = QWidget()
-        grid: QGridLayout = QGridLayout()
-        text_edit = QTextEdit()
+        self.root = QWidget()
+        self.root_layout: QGridLayout = QGridLayout()
+        self.root.setLayout(self.root_layout)
+        self.root.setGeometry(200, 200, 1200, 800)
+        self.root.setWindowTitle("PersonalWiki")
+
+        editor_splitter: QSplitter = QSplitter(parent=self.root)
+        self.root_layout.addWidget(editor_splitter, 0, 0)
+
+        text_edit = QTextEdit(editor_splitter)
         text_edit.setText("Hello World!")
         text_edit.setAcceptRichText(False)
-        grid.addWidget(text_edit)
-        self.widget.setLayout(grid)
-        self.widget.setGeometry(200, 200, 1200, 800)
-        self.widget.setWindowTitle("PersonalWiki")
+        editor_splitter.addWidget(text_edit)
         
+        text_view = QWebEngineView(self.root)
+        text_view.load(QUrl("http://www.qt.io"))
+        text_view.show()
+        editor_splitter.addWidget(text_view)
 
     def run(self):
-        self.widget.show()
+        """
+            Runs the app
+        """
+        self.root.show()
         sys.exit(self.app.exec())
