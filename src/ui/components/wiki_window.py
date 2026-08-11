2c8b4b085e3d9e73d8fc8489970253a72caddc12
Your Name <your.email@example.com>
2026-08-11 11:43:29 +0800

    added file dialog

--- src/ui/components/wiki_window.py
+++ src/ui/components/wiki_window.py
@@ -1,4 +1,7 @@
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
+from typing import override
+
+from PyQt6.QtGui import QCloseEvent
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog
 
 
 class WikiWindow(QMainWindow):
@@ -12,9 +15,18 @@ class WikiWindow(QMainWindow):
         panel.setLayout(QVBoxLayout())
 
         new_button = QPushButton(parent=panel, text="New Wiki")
+        if (l := panel.layout()) is not None:
+            l.addWidget(new_button)
 
         open_button = QPushButton(parent=panel, text="Open Wiki")
-
-        if l := panel.layout():
-            l.addWidget(new_button)
+        open_button.clicked.connect(self.on_open_btn_clicked)
+        if (l := panel.layout()) is not None:
             l.addWidget(open_button)
+
+
+
+    def on_open_btn_clicked(self):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+        file_dialog.setNameFilter("wiki.pwi")
+        file_dialog.exec()
