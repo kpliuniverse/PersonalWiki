204c763a2d98147a98e058dca80fb745fd3ff2f7
kpliuniverse <lolitalaroasas@gmail.com>
2026-08-29 11:53:10 +0800

    added documentation

--- src/app.py
+++ src/app.py
@@ -1,14 +1,13 @@
 
+import sys
+import pathlib 
+import logging
+
 from PyQt6.QtWidgets import (
     QApplication,
 )
 
 from PyQt6.QtGui import QFontDatabase
-
-import sys
-import pathlib 
-import logging
-
 from src.initcontext import InitContext
 from src.ui.components.main_window import MainWindow
 from src.ui.components.wiki_window import WikiWindow
@@ -18,7 +17,10 @@ FONT_PATH = pathlib.Path("assets/fonts")
 
 class App:
 
-    def init_fonts(self): 
+    def init_fonts(self):
+        """
+            Initialize font to the application's resident QFontDatabase
+        """
         for path in FONT_PATH.iterdir():
             QFontDatabase.addApplicationFont(path.as_posix())
             logging.info("Added font file: %s", path.as_posix())
@@ -28,12 +30,18 @@ class App:
         self.app = QApplication(sys.argv)
 
     def run_main(self, wiki: pathlib.Path):
+        """
+            Runs the main windows.
+        """
         self.main_window = MainWindow(initcontext=InitContext(
             path_to_pwi_file=pathlib.Path(wiki)
         ))
         self.main_window.show()
 
     def run(self):
+        """
+            Runs the app
+        """
         self.init_fonts()
         if len(self.app.arguments()) == 1:
             wiki_window = WikiWindow()
