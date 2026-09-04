10795158fddc0656a9f4e191d6c13d26ff172d12
kpliuniverse <lolitalaroasas@gmail.com>
2026-09-04 21:16:32 +0800

    now webserver works

--- webserver_main.py
+++ webserver_main.py
@@ -5,17 +5,21 @@ from werkzeug import Request, Response
 from werkzeug.exceptions import HTTPException, NotFound
 from werkzeug.routing import Map, Rule
 from werkzeug.serving import run_simple
+from werkzeug.middleware.shared_data import SharedDataMiddleware
+from src.templating.templating import MainHTMLTemplater
 
 
 class WebServer(object):
-    def view(self, md):
-        return Response(str(md))   
+    def view(self, args):
+        context = {
+            "body": args["md"]
+        }
+        return Response(MainHTMLTemplater().render(context), mimetype="text/html")
 
     def wsgi_app(self, environ, start_response):
         self.url_map = Map([
             Rule('/view/<md>', endpoint='view'),
         ])
-
         request = Request(environ)
         adapter = self.url_map.bind_to_environ(request.environ)
 
@@ -37,5 +41,11 @@ class WebServer(object):
 # create_environ(".testenv/wikis/basic", "http://localhost:8080")
 
 
+def create_app():
+    app = WebServer()
+    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+        '/default_static':  (pathlib.Path(__file__).parent / "src/static").as_posix()
+    })
+    return app
 if __name__ == "__main__": 
-    run_simple("127.0.0.1", 8080, WebServer())
+    run_simple("127.0.0.1", 8080, create_app())
