5a810e225af22fce1c79377b8032fc60eabd8a0d
kpliuniverse <lolitalaroasas@gmail.com>
2026-09-08 09:58:21 +0800

    made pytest call `multiprocessing.freeze_support` before testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+import logging
 import os
 import sys
 
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if ROOT not in sys.path:
-    sys.path.insert(0, ROOT)
+def pytest_configure(config):
+    import multiprocessing
+    multiprocessing.freeze_support()
+    ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    if ROOT not in sys.path:
+        sys.path.insert(0, ROOT)
