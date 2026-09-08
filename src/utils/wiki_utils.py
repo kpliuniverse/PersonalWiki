cfb52c58e52634f1f9f35242516c6ab4dd748fa0
TECH301 <LAB 2@DESKTOP-FA7JF2A>
2026-09-08 17:06:45 +0800

    wip

--- src/utils/wiki_utils.py
+++ src/utils/wiki_utils.py
@@ -1,13 +1,19 @@
 import logging
 import os
 import pathlib
-from typing import Optional
+from typing import Optional, List
 
-from src.wiki.wiki_items import UnnamedFolderItem, FileItem
+from src.wiki.wiki_items import UnnamedFolderItem, FileItem, NamedFolderItem, NamedItem
 
 
 def walk_and_return_folder_item(root_path: pathlib.Path):
-    root_folder_item = UnnamedFolderItem("root")
+    root_folder_item = NamedFolderItem("root")
     cur_folder_stack = [root_folder_item]
     prev_path: Optional[pathlib.Path] = None
-    for (path, _, filenames) in os.walk(root_path):
+    for (path, dirnames, filenames) in os.walk(root_path):
+        items: List[NamedItem] = []
+        items.extend([NamedFolderItem(n) for n in dirnames])
+        items.extend([FileItem(n) for n in filenames])
+        items.sort(key=lambda item: item.name())
+        for item in items:
+            cur_folder_stack[-1].add_child(item)
