51af2ad46a608dcb0480fe61787a3bbd9985ed7c
kpliuniverse <lolitalaroasas@gmail.com>
2026-09-23 14:38:01 +0800

    forgot to alter tests to use new api

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -5,9 +5,9 @@ from src.multiprocessing.child_processes.webserver import WebserverProcess
 
 from returns.result import Failure, Success
 
-def test_close():
+def test_close(tmp_path):
     mgr = multiprocessing.MultiprocessingManager()
-    match mgr.run_process(WebserverProcess()):
+    match mgr.run_process(WebserverProcess(tmp_path)):
         case Failure(e):
             raise multiprocessing.MultiprocessingException(e)
         case Success(i_d):
